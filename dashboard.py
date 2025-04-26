e10cdef148f3c68c4c0763104e64ed9d5bce6bab
Iffath Saleem <iffathsaleem101@gmail.com>
2025-04-26 22:14:23 +0530

    library imported

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from categories import categories, map_category
+import plotly.graph_objects as go
 
 # Background image configuration
 background_images = {
